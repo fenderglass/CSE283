0d4200e1a6cffa5aaf2b2f7d9085e3dab8dbb9ce
Mikhail Kolmogorov <fenderglass@gmail.com>
2015-06-12 19:46:42 -0700

    aa

--- svm.py
+++ svm.py
@@ -41,7 +41,7 @@ def read_network(filename):
     return edges
 
 
-def get_features(mutations, sample_labels, network):
+def get_features(mutations, sample_labels, network, num_iters):
     gene_ids = {}
     cur_id = 0
     for genes in mutations.itervalues():
@@ -59,13 +59,13 @@ def get_features(mutations, sample_labels, network):
         for neighbor in neighbors:
             adj_matrix[gene_ids[node]][gene_ids[neighbor]] = 1
 
-    print("Propagating")
+    #print("Propagating")
     features = np.zeros((len(mutations), cur_id))
     labels = np.zeros(len(mutations))
     for sample_id, sample in enumerate(mutations):
         #print(sample_id, "/", len(mutations))
         genes = set(map(gene_ids.get, mutations[sample]))
-        weights = propagate(genes, adj_matrix)
+        weights = propagate(genes, adj_matrix, num_iters)
 
         for gene_id, weight in enumerate(weights):
             features[sample_id][gene_id] = weight
@@ -89,15 +89,14 @@ def split_training_set(features, labels, rate):
             np.array(second_fetures), np.array(second_labels))
 
 
-def propagate(start_nodes, adj_matrix):
-    ALPHA = 0.6
-    ITERS = 2
+def propagate(start_nodes, adj_matrix, num_iters):
+    ALPHA = 0.9
 
     values = np.zeros((1, adj_matrix.shape[0]))
     for node in start_nodes:
         values[0][node] = 1
 
-    for it_num in xrange(ITERS):
+    for it_num in xrange(num_iters):
         values = ALPHA * values.dot(adj_matrix)
         for node in start_nodes:
             values[0][node] += 1 - ALPHA
@@ -106,7 +105,7 @@ def propagate(start_nodes, adj_matrix):
 
 
 def train_svm(features, labels):
-    print("Training SVM")
+    #print("Training SVM")
     norm_features = preprocessing.scale(features)
     train_features, train_labels, test_features, test_labels = \
                                 split_training_set(norm_features, labels, 0.4)
@@ -118,8 +117,7 @@ def train_svm(features, labels):
         for j in xrange(features.shape[1]):
             svm_features[j] += clf.dual_coef_[0][i] * clf.support_vectors_[i][j]
 
-    print("Score:", clf.score(test_features, test_labels))
-    return svm_features
+    return svm_features, clf.score(test_features, test_labels)
 
 
 def print_features(svm_features, gene_ids):
@@ -127,25 +125,49 @@ def print_features(svm_features, gene_ids):
     for gene, g_id in gene_ids.items():
         id_2_gene[g_id] = gene
 
-    print("Main features:")
+    #print("Main features:")
     svm_main = sorted(enumerate(svm_features),
                       key=lambda x: abs(x[1]), reverse=True)
     for f_id, f_val in svm_main[:20]:
         print("{0}\t{1:5.2f}".format(id_2_gene[f_id], f_val))
 
 
-def main():
-    network = read_network(sys.argv[1])
+def make_classifier(network_file, maf_files, num_iters):
+    network = read_network(network_file)
     labels = {}
     mutations = {}
-    for maf_no, maf_file in enumerate(sys.argv[2:]):
+    for maf_no, maf_file in enumerate(maf_files):
         sample_mutations = read_maf(maf_file)
         mutations.update(sample_mutations)
         labels.update({l : maf_no for l in sample_mutations})
-    features, labels, gene_ids = get_features(mutations, labels, network)
-    svm_features = train_svm(features, labels)
-    print_features(svm_features, gene_ids)
+    features, labels, gene_ids = get_features(mutations, labels,
+                                              network, num_iters)
+    svm_features, score = train_svm(features, labels)
+    #print_features(svm_features, gene_ids)
+    return score
 
+def main():
+    if len(sys.argv) < 4:
+        print("Usage: svm.py prot_network sample_1 sample_2 [sample_3...]")
+        return 1
+
+    #score = make_classifier(sys.argv[1], sys.argv[2:], 1)
+    #print("Score: {0:5.2f}".format(score))
+
+    NUM_EXPERIMENTS = 30
+    without_scores = []
+    with_scores = []
+    for i in xrange(NUM_EXPERIMENTS):
+        score = make_classifier(sys.argv[1], sys.argv[2:], 0)
+        without_scores.append(score)
+
+    for i in xrange(NUM_EXPERIMENTS):
+        score = make_classifier(sys.argv[1], sys.argv[2:], 1)
+        with_scores.append(score)
+
+    print("Naive\tPropagation")
+    for fst, snd in zip(without_scores, with_scores):
+        print("{0}\t{1}".format(fst, snd))
 
 if __name__ == "__main__":
     main()
